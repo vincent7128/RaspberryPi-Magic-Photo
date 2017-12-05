5f465bb61aa7481c446798297a33f0cafe0e56d0
Vincent Chen <vincent@pinpin.link>
2017-12-06 00:20:07 +0800

    use gif animation

--- www/js/index.js
+++ www/js/index.js
@@ -1,37 +1,32 @@
-var photoA, photoB, postFrame, interval;
+var png, gif, frame;
 
 function animationStart() {
-    interval = setInterval(function () {
-        if (photoA.style.getPropertyValue('display')) {
-            photoA.style.setProperty('display', '');
-            photoB.style.setProperty('display', 'block');
-        } else {
-            photoA.style.setProperty('display', 'block');
-            photoB.style.setProperty('display', '');
-        }
-    }, 300);
+    png.style.setProperty('display', '');
+    gif.style.setProperty('display', 'block');
 }
 
 function animationStop() {
-    removeInterval(interval)
+    png.style.setProperty('display', 'block');
+    gif.style.setProperty('display', '');
 }
 
 function adjustPosition() {
-    var top = (window.innerHeight - postFrame.height) / 2,
-        left = (window.innerWidth - photoA.width) / 2;
-    photoA.style.setProperty('top', top + 306 + 'px');
-    photoA.style.setProperty('left', left + 'px');
-    photoB.style.setProperty('top', top + 306 + 'px');
-    photoB.style.setProperty('left', left + 'px');
-    postFrame.style.setProperty('top', top + 'px');
+    var top = (window.innerHeight - frame.height) / 2,
+        left = (window.innerWidth - png.width) / 2;
+    png.style.setProperty('top', top + 306 + 'px');
+    png.style.setProperty('left', left + 'px');
+    gif.style.setProperty('top', top + 306 + 'px');
+    gif.style.setProperty('left', left + 'px');
+    frame.style.setProperty('top', top + 'px');
 }
 
 window.onload = function () {
-    photoA = document.querySelector('.photo-a');
-    photoB = document.querySelector('.photo-b');
-    postFrame = document.querySelector('.poster-frame');
+    png = document.querySelector('.png');
+    gif = document.querySelector('.gif');
+    frame = document.querySelector('.frame');
     adjustPosition();
-    animationStart();
+    setTimeout(animationStart, 1000);
+    setTimeout(animationStop, 10000);
 };
 
 window.onresize = adjustPosition;
