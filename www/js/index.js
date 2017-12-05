7edbeaa24be7227a1b46b6c8686eef012d4e2727
Vincent Chen <vincent@pinpin.link>
2017-12-05 22:09:03 +0800

    addjust photo position

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
-function load() {
-    var photoA = document.querySelector('.photo-a');
-    var photoB = document.querySelector('.photo-b');
-    var intr = setInterval(function() {
+var photoA, photoB, postFrame, interval;
+
+function animationStart() {
+    interval = setInterval(function () {
         if (photoA.style.getPropertyValue('display')) {
             photoA.style.setProperty('display', '');
             photoB.style.setProperty('display', 'block');
@@ -12,4 +12,26 @@ function load() {
     }, 300);
 }
 
-window.onload = load;
+function animationStop() {
+    removeInterval(interval)
+}
+
+function adjustPosition() {
+    var top = (window.innerHeight - postFrame.height) / 2,
+        left = (window.innerWidth - photoA.width) / 2;
+    photoA.style.setProperty('top', top + 306 + 'px');
+    photoA.style.setProperty('left', left + 'px');
+    photoB.style.setProperty('top', top + 306 + 'px');
+    photoB.style.setProperty('left', left + 'px');
+    postFrame.style.setProperty('top', top + 'px');
+}
+
+window.onload = function () {
+    photoA = document.querySelector('.photo-a');
+    photoB = document.querySelector('.photo-b');
+    postFrame = document.querySelector('.poster-frame');
+    adjustPosition();
+    animationStart();
+};
+
+window.onresize = adjustPosition;
